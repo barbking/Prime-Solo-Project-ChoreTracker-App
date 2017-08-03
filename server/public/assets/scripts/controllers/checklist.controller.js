8f231ba1b298e02f63dd2329b6ba9f3fb0a864cd
Barb King <barbgross@aol.com>
2017-08-03 17:26:47 -0500

    moved progress bar to task page

--- server/public/assets/scripts/controllers/checklist.controller.js
+++ server/public/assets/scripts/controllers/checklist.controller.js
@@ -8,19 +8,65 @@ myApp.controller('CheckListController', [ '$http', '$location','tasksService', f
           // user has a curret session on the server
           vm.firstname = response.data.firstname;
           vm.userName = response.data.username;
-          console.log('vm.userName: ', vm.userName);
+          vm.allowance = response.data.allowance;
+          console.log('get user response: ', response.data);
           //get username specific task using tasksService service
           tasksService.getUserTasks(vm.userName).then(function(){
             vm.usertasks = tasksService.usertasks;
             console.log('get usertasks:', vm.usertasks);
+            vm.calcReward();
           }); //get tasks for this specific username
       } else {
           // user has no session, bounce them back to the login page
           $location.path("/home");
       }
   });//end of $http get
+  //data for progress bar (tasks completed/allwance calc percent)
+  vm.calcReward = function (){
+    var completedTasks = 0;
+    var required = 0;
+    for (i=0; i<=vm.usertasks.length-1; i++){
+      completedTasks += parseInt(vm.usertasks[i].checkboxcount);
+      required += parseInt(vm.usertasks[i].frequency);
+    }
+    vm.greenDone = (completedTasks/required)*100;
+    console.log('green pie graph %', vm.greenDone);
+    vm.allowanceEarned=((vm.greenDone/100)*(vm.allowance)).toFixed(0);
+    console.log(vm.allowanceEarned);
+    vm.progressPercent = 'width:' + vm.greenDone.toFixed(0) + '%';
+    console.log(vm.progressPercent);
+    vm.data = (vm.allowanceEarned,vm.progressPercent);
+    return vm.data;
+  };//end of calcReward
   //on checkbox click, run saveCheck func which calls service updateTask http PUT
-  vm.saveCheck = tasksService.updateTasks;
+  // vm.saveCheck = tasksService.updateTasks;
+  // update task checkbox status upon box click and save
+   vm.saveCheck = function (task) {
+     console.log('in updateTask http request:', task);
+    //counter to tack number of true/false checkboxes up to limit and save in db as checkboxcount
+    counter = 0;
+     for (var key in task){
+       limit = task.frequency;
+       console.log('limit:', limit);
+       if (task[key] === true && counter < limit) {
+         counter += 1;
+       }
+       task.checkboxcount = counter;
+       console.log(task.checkboxcount);
+     }
+      return $http({
+       method: 'POST',
+       url: '/tasks/checkbox',
+       data: task
+     }).then(function(response){
+       console.log(response);
+       tasksService.getUserTasks(vm.userName).then(function(){
+         vm.usertasks = tasksService.usertasks;
+         console.log('get usertasks:', vm.usertasks);
+         vm.calcReward();
+       }); //get tasks for this specific username
+     });
+   };//end updateTasks func
   //user logout when logout button clicked
   vm.logout = function() {
     $http.get('/user/logout').then(function(response) {
